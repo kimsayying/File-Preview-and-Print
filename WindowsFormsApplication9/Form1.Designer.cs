7e477ea4aa89551820439211876cc4ca6038c028
sykim <kimsayying93@gmail.com>
2018-03-07 15:57:11 +0800

    Add project files.

---
+++ WindowsFormsApplication9/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WindowsFormsApplication9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.filesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtOld = new System.Windows.Forms.ToolStripTextBox();
+            this.txtNew = new System.Windows.Forms.ToolStripTextBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printPreviewPrintToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filesToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(883, 45);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // filesToolStripMenuItem
+            // 
+            this.filesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.printPreviewPrintToolStripMenuItem,
+            this.printToolStripMenuItem});
+            this.filesToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.filesToolStripMenuItem.Name = "filesToolStripMenuItem";
+            this.filesToolStripMenuItem.Size = new System.Drawing.Size(70, 41);
+            this.filesToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(159, 42);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(159, 42);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "rtf";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "RTF files|*.rtf|All files|*.*";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "rtf";
+            this.saveFileDialog1.Filter = "RTF files|*.rtf|All files|*.*";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.richTextBox1.Location = new System.Drawing.Point(0, 45);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(883, 593);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtOld,
+            this.txtNew,
+            this.fontToolStripMenuItem});
+            this.editToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(75, 41);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // txtOld
+            // 
+            this.txtOld.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.txtOld.Name = "txtOld";
+            this.txtOld.Size = new System.Drawing.Size(100, 34);
+            this.txtOld.DoubleClick += new System.EventHandler(this.txtOld_DoubleClick);
+            // 
+            // txtNew
+            // 
+            this.txtNew.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.txtNew.Name = "txtNew";
+            this.txtNew.Size = new System.Drawing.Size(100, 34);
+            this.txtNew.DoubleClick += new System.EventHandler(this.txtNew_DoubleClick);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(160, 42);
+            this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click_1);
+            // 
+            // printPreviewPrintToolStripMenuItem
+            // 
+            this.printPreviewPrintToolStripMenuItem.Name = "printPreviewPrintToolStripMenuItem";
+            this.printPreviewPrintToolStripMenuItem.Size = new System.Drawing.Size(314, 42);
+            this.printPreviewPrintToolStripMenuItem.Text = "Print Preview/Print";
+            this.printPreviewPrintToolStripMenuItem.Click += new System.EventHandler(this.printPreviewPrintToolStripMenuItem_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.AllowSelection = true;
+            this.printDialog1.AllowSomePages = true;
+            this.printDialog1.Document = this.printDocument1;
+            this.printDialog1.ShowHelp = true;
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(314, 42);
+            this.printToolStripMenuItem.Text = "Print";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 638);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem filesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox txtOld;
+        private System.Windows.Forms.ToolStripTextBox txtNew;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printPreviewPrintToolStripMenuItem;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication9/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e) //code runs to save file to hard drive
+        {
+            // allow us to save file,file is saved in the location selected in the file save dialog 
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+               // if richTextBox.text is empty string                
+                if(richTextBox1.Text == "")
+                {
+                    MessageBox.Show("Please type something to save!!");
+                }
+                else
+                {
+                    richTextBox1.SaveFile(saveFileDialog1.FileName); // run and save the file text to an actual file
+
+                    MessageBox.Show("File saved!!");
+                    richTextBox1.Text = "";
+                }   
+               
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) //code runs to open file from hard drive
+        {
+            openFileDialog1.FileName = ""; //Load dialog name with no file name
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    richTextBox1.LoadFile(openFileDialog1.FileName); // load file from drive to rich text box
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"{ex.Message} Please try open another file.");//shows the message from the exception object
+                }
+            }                                
+        }
+
+        private void txtOld_DoubleClick(object sender, EventArgs e) //code runs to find the matched value
+        {   
+            if (txtOld.Text.Length > 0)
+            {
+                richTextBox1.Find(txtOld.Text);
+                richTextBox1.SelectionBackColor = Color.DeepSkyBlue;
+                MessageBox.Show($"{richTextBox1.SelectedText}");
+                //richTextBox1.SelectedText = txtNew.Text;
+            }
+        }
+
+        private void txtNew_DoubleClick(object sender, EventArgs e) // code runs to replace the matched value to new value 
+        {
+            if (txtNew.Text != "")
+            {
+                richTextBox1.Text = richTextBox1.Text.Replace(txtOld.Text, txtNew.Text);
+            }
+        }
+
+        private void fontToolStripMenuItem_Click_1(object sender, EventArgs e) // code runs to change to font format
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK) // show a box to show font dialog
+            {
+                richTextBox1.Font = fontDialog1.Font;    // richtextBox font set to the font chosen
+                MessageBox.Show($"{fontDialog1.Font.Name}");
+            }            
+        }
+
+        private void printPreviewPrintToolStripMenuItem_Click(object sender, EventArgs e) //code runs to preview pages using the PrintPreviewDialog control
+        {
+            printPreviewDialog1.Document = printDocument1;//choose document to be display in the preview panel
+            printPreviewDialog1.ShowDialog();//brings up a little window to show a preview
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e) //code behind the scene
+        {
+            //code runs when a page is about to be preview or a page is about to be printed
+            // Draws the string within the bounds of the page.
+            e.Graphics.DrawString(richTextBox1.Text, fontDialog1.Font, Brushes.Black,
+            e.MarginBounds, StringFormat.GenericTypographic);
+        }
+
+        //document properties set to printDocument1
+        private void printToolStripMenuItem_Click(object sender, EventArgs e) // code runs when printing
+        {
+            if(printDialog1.ShowDialog() == DialogResult.OK)
+            {
+                printDocument1.Print(); // call the print method to print the document
+            }
+           
+        }
+    }
+}
